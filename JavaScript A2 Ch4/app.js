43dae12d9a33979e6cd8d05ceac0be77b5065d05
M. Mudassir Awan <121383917+Mudasssirr@users.noreply.github.com>
2023-01-29 11:01:48 +0500

    added JS A2 Ch4
    added JavaScript Assignment 2 from Chapter 4

---
+++ JavaScript A2 Ch4/app.js
@@ -0,0 +1,24 @@
+//Chapter 4
+
+//Question 1
+//var variableOne variableTwo variableThree;
+
+//Question 2
+//var variable1;
+//var $variable2;
+//var _variable3;
+//var v1234;
+//var variableFive;
+
+//var variable 6;
+//var "variable7";
+//var 42438;
+//var alert 9;
+//var variable-10;
+
+//Question 3
+//document.write ("<h3>Rules for naming JS variables</h3>");
+//document.write ("<p>Variable names can only contain names, numbers, dollar signs and underscores. For Example:$my_1stVariable.</p>");
+//document.write ("<p>Variables must begin with a $ (dollarsign), _ (underscore), or name. For Example:$name, _name or name.</p>");
+//document.write ("<p>Variable names are case sensitive</p>");
+//document.write ("<p>Variable names should not be JS keywords</p>");
